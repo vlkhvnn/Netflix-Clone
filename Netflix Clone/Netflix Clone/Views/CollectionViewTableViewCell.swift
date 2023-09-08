82f9646aea6274ea02ed5c0b5672be341db2ab59
Alikhan Tangirbergen <tanir.alikh@gmail.com>
2023-09-09 03:46:02 +0600

    Setting HomeViewController TableView

---
+++ Netflix Clone/Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewCell.swift
+//  Netflix Clone
+//
+//  Created by Alikhan Tangirbergen on 09.09.2023.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
