bcbcdefa2c746580ba503be2eaebd40e2720bbbf
Alikhan Tangirbergen <tanir.alikh@gmail.com>
2023-09-18 02:59:26 +0600

    Creating TitleCollectionCell

---
+++ Netflix Clone/Netflix Clone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  CollectionViewCell.swift
+//  Netflix Clone
+//
+//  Created by Alikhan Tangirbergen on 18.09.2023.
+//
+
+import UIKit
+import SDWebImage
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    static let identifier = "TitleCollectionViewCell"
+    
+    private let posterImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+    
+    public func configure(with model: String) {
+        guard let url = URL(string: model) else {return}
+        posterImageView.sd_setImage(with: url)
+    }
+}
